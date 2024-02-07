ca7d5846dc71631f5a69d543fb02b49336bd0b0a
tanaka <lemo.nade.engineer.room@gmail.com>
2024-02-07 10:34:28 +0900

    Vapor, Fluent, WebAuthnの依存を追加

--- Package.swift
+++ Package.swift
@@ -15,12 +15,24 @@ let package = Package(
     dependencies: [
         // DocC
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.89.0"),
+        // 🗄 An ORM for SQL and NoSQL databases.
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
+        // Passkey
+        .package(url: "https://github.com/swift-server/webauthn-swift.git", from: "1.0.0-alpha"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Passkey"),
+            name: "Passkey",
+            dependencies: [
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "WebAuthn", package: "webauthn-swift"),
+            ]
+        ),
         .testTarget(
             name: "PasskeyTests",
             dependencies: ["Passkey"]),
